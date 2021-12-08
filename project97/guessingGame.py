6a21f4a331755c8777824445734a33c078ed2674
trishna15 <77284959+trishna15@users.noreply.github.com>
2021-12-08 20:52:25 +0530

    Add files via upload

---
+++ project97/guessingGame.py
@@ -0,0 +1,18 @@
+import random
+print("Number guessing game")
+print("Guess a number (between 1 and 9 )")
+number = random.randint(1,9)
+chances = 0
+while chances < 5 :
+    guess = int(input("Enter your guess :-"))
+    if guess == number :
+        print("Congratulations YOU WON!!!")
+        break
+    elif guess < number :
+       print("Your guess was too low: Guess a number higher than", guess)
+       chances += 1
+    else :
+        print("Your guess was too high: Guess a number lower than", guess)
+        chances += 1
+if not chances < 5:
+    print("YOU LOSE!!! The number is", number)
